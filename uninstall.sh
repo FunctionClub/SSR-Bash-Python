94bd7e8caf4ba0ef3f7b68c4404345a040eb6086
Admin-backups <wwb601237044@hotmail.com>
2017-07-28 21:53:17 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,5 @@
-﻿#!/bin/bash
+#!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 
@@ -8,6 +7,9 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 bash /usr/local/shadowsocksr/stop.sh
 
 #Delete Files
-rm -rf /usr/local/SSR-Bash-Python
+rm -rf /usr/local/jluee
+echo 'rm -rf /usr/local/jluee'
 rm -rf /usr/local/shadowsocksr
+echo 'rm -rf /usr/local/shadowsocksr'
 rm -rf /usr/local/bin/ssr
+echo 'rm -rf /usr/local/bin/ssr'
