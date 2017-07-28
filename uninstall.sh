fca63e0a71b344651d9492f902e524d9e3f46729
Admin-backups <wwb601237044@hotmail.com>
2017-07-28 22:11:03 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,13 +3,10 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 
-#Stop ShadowsocksR
-bash /usr/local/shadowsocksr/stop.sh
-
 #Delete Files
-rm -rf /usr/local/jluee
-echo 'rm -rf /usr/local/jluee'
+rm -rf /usr/local/SSR-Bash-Python
+echo '已删除控制脚本'
 rm -rf /usr/local/shadowsocksr
-echo 'rm -rf /usr/local/shadowsocksr'
+echo '已删除服务脚本'
 rm -rf /usr/local/bin/ssr
-echo 'rm -rf /usr/local/bin/ssr'
+echo '已删除全部脚本！感谢您的支持'
